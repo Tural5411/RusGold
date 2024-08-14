4b6c2baa0707219bb65e01d3e5926083d20a466c
Tural <tural_5411@mail.ru>
2024-08-15 00:31:29 +0400

    fx

--- RusGold.Entities/Concrete/Car.cs
+++ RusGold.Entities/Concrete/Car.cs
@@ -12,20 +12,12 @@ namespace RusGold.Entities.Concrete
     public class Car:EntityBase,IEntity
     {
         public string Name { get; set; }
-        public int ModelId { get; set; }
-        public int BrendId { get; set; }
+        public int CategoryId { get; set; }
+        public string PriceByCard { get; set; }
         public string Price { get; set; }
-        public string HorsePower { get; set; }
+        public bool IsGold { get; set; }
         public string Content { get; set; }
-        public string TechnicalParameters { get; set; }
         public string ThumbNail { get; set; }
-        public string Body { get; set; }
-        public string FuelType { get; set; }
-        public string Year { get; set; }
-        public string Transmission { get; set; }
-        public string DriveType { get; set; }
-        public string Color { get; set; }
-        public string EngineSize { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
     }
--- RusGold.Entities/Concrete/CarBrendModel.cs
+++ RusGold.Entities/Concrete/CarBrendModel.cs
@@ -12,5 +12,6 @@ namespace RusGold.Entities.Concrete
         public int? ParentId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Thumbnail { get; set; }
     }
 }
